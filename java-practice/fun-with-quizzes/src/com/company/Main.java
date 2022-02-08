a6c5a378349d53ba866a5f8d440f3e241eacba96
System Administrator <root@Eswaramoorthis-MacBook-Air.local>
2022-02-08 18:36:47 -0500

    Fun with quiz

---
+++ java-practice/fun-with-quizzes/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Quiz quiz = new Quiz();
+        quiz.addQuestion(new Question("How many coasters are at Carowinds?", "13"));
+        quiz.addQuestion(new Question("Which sea creature has 3 hearts?", "Octopus"));
+        quiz.addQuestion(new Question("What is the Italian word for Pie?", "Pizza"));
+
+        quiz.runQuiz();
+    }
+
+}
